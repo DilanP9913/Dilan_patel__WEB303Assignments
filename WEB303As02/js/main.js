49d52c473ae4c9c342cc433e20438aebb9b0cc0c
DilanP9913 <105453257+DilanP9913@users.noreply.github.com>
2022-09-28 22:04:14 -0400

    Add files via upload

---
+++ WEB303As02/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function()
+{
+    $("#prospect").on("click", function()
+        {        
+        $("#content").empty();
+        $("#content").hide().fadeIn(1000);
+        $("#content").load("./prospect.html");
+        $("#content").css("display:block");
+    });
+    $("#convert").on("click", function()
+    {
+        $("#content").empty();
+        $("#content").hide().fadeIn(1000);
+        $("#content").load("./convert.html");
+        $("#content").css("display:block");
+
+    });
+    $("#retain").on("click", function()
+    {
+        $("#content").empty();
+        $("#content").hide().fadeIn(1000);
+        $("#content").load("./retain.html");
+        $("#content").css("display:block");
+    });
+});
+
+/* 
+Name: Dilankumar Patel
+Student Id: 0775508
+*/
